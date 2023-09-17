b952f0304c0ce2087c59c954efa53d9a8814f266
JunrenWang <junrenw2@illinois.edu>
2023-09-17 09:36:34 -0500

    sort by water capacity

--- public/socketClient.js
+++ public/socketClient.js
@@ -50,18 +50,88 @@ socket.on('updateOrder',currentorder=>{
 
 socket.on('updateLeaderBoard',leaderBoardArray=>{
     // console.log(leaderBoardArray)
-    leaderBoardArray.sort((a,b)=>{
-        return b.score - a.score;
-    })
-    document.querySelector('.leader-board').innerHTML = "";
-    leaderBoardArray.forEach(p=>{
-        if(!p.name){
-            return;
-        }
-        document.querySelector('.leader-board').innerHTML += `
-            <li class="leaderboard-player">${p.name} - ${p.score}</li>
-        `
-    })
+    if (document.getElementById("sort-order").classList.contains('active')){
+        leaderBoardArray.sort((a,b)=>{
+            return b.score - a.score;
+        })
+        document.querySelector('.leader-board').innerHTML = "";
+        leaderBoardArray.forEach(p=>{
+            if(!p.name){
+                return;
+            }
+            document.querySelector('.leader-board').innerHTML += `
+                <li class="leaderboard-player">${p.name} - ${p.score}</li>
+            `
+        })
+    }
+    if (document.getElementById("sort-wc").classList.contains('active')){
+        leaderBoardArray.sort((a,b)=>{
+            return b.wc - a.wc;
+        })
+        document.querySelector('.leader-board').innerHTML = "";
+        leaderBoardArray.forEach(p=>{
+            if(!p.name){
+                return;
+            }
+            document.querySelector('.leader-board').innerHTML += `
+                <li class="leaderboard-player">${p.name} - ${(p.wc).toFixed(2)}</li>
+            `
+        })
+    }
+    
+  
+
+    const sortOptions = document.querySelectorAll('.sort-option');
+    sortOptions.forEach(option => {
+        option.addEventListener('click', function() {
+            // First, remove the 'active' class from all options
+            sortOptions.forEach(opt => {
+                opt.classList.remove('active');
+            });
+    
+            // Add 'active' class to the clicked option
+            this.classList.add('active');
+
+            if (this.id == "sort-order"){
+                leaderBoardArray.sort((a,b)=>{
+                    return b.score - a.score;
+                })
+            
+                document.querySelector('.leader-board').innerHTML = "";
+                leaderBoardArray.forEach(p=>{
+                    if(!p.name){
+                        return;
+                    }
+                    document.querySelector('.leader-board').innerHTML += `
+                        <li class="leaderboard-player">${p.name} - ${p.score}</li>
+                    `
+                })
+            }
+
+            if (this.id == "sort-wc"){
+                leaderBoardArray.sort((a,b)=>{
+                    return b.wc - a.wc;
+                })
+            
+                document.querySelector('.leader-board').innerHTML = "";
+                leaderBoardArray.forEach(p=>{
+                    if(!p.name){
+                        return;
+                    }
+                    document.querySelector('.leader-board').innerHTML += `
+                        <li class="leaderboard-player">${p.name} - ${(p.wc).toFixed(2)}</li>
+                    `
+                })
+            }
+
+        });
+    });
+    
+
+
+
+
+
     const el = leaderBoardArray.find(u=>u.name === player.name)
     document.querySelector('.player-score').innerHTML = el.score
     document.querySelector('.player-watercapacity').innerHTML = (el.wc).toFixed(2)
